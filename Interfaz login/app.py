13a73cbb165ccb8e0de30ff516354e9a04b6a6f1
Andres <andresf.rojass@gmail.com>
2020-12-18 13:40:37 -0500

    Ajuste App.py Render

--- Interfaz login/app.py
+++ Interfaz login/app.py
@@ -15,26 +15,26 @@ def registro():
 def busqueda():
     return render_template('busquedaImagenes.html')
 
-@app.route('/sesion/', methods=['GET', 'POST'])
-def sesion():    
+@app.route('/sesion/', methods=['GET','POST'])
+def sesion():
     #try:
       if request.method == 'POST':
          usuario = request.form['usuario']
          clave = request.form['contraseña']
          email = request.form['correo']
-         if utils.isEmailValid(email):         
-            if utils.isUsernameValid(usuario):            
+         if utils.isEmailValid(email):
+            if utils.isUsernameValid(usuario):
                yag = yagmail.SMTP('penarandah@uninorte.edu.co','TuClavePersonal')
                yag.send(to=email,subject='Validar cuenta',
-               contents='Revisa tur correo para activar tu cuenta.') 
+               contents='Revisa tu correo para activar tu cuenta.')
                return "Correo enviado a:  " + email
             else:
                return "Usuario no valido.  " + usuario
          else:
             return "Correo no valido.  " + usuario
-      else:         
-          return 'Entra con GET'
-    #except: 
+      else:
+          return render_template('sesion.html')
+    #except:
        #return render_template('sesion.html')
 
 @app.route('/gestorimagen/')
@@ -58,4 +58,3 @@ def eliminar():
 @app.route('/gestorimagen/descargar/')
 def descargar():
     return render_template('descargar.html')
-
