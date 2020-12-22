45c8a711c9a9f9c2ff317fd9653a3899bf0e8b6b
Cristian Vitola <chriss.vian@gmail.com>
2020-12-22 16:08:25 -0500

    arreglo cargar archivos

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ from werkzeug.utils import secure_filename # para obtener el nombre del archivo 
 app = Flask(__name__)
 
 # Codigo para crear token de seguridad y crear carpeta de imagenes
-FOLDER_CARGA = os.path.abspath("static/resources") # carpeta donde se cargarán las imágenes.
+#FOLDER_CARGA = os.path.abspath("static/resources") # carpeta donde se cargarán las imágenes.
 SECRET_KEY = os.urandom(32) # Para generar la llave aleatoria
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config["FOLDER_CARGA"] = FOLDER_CARGA
+app.config["UPLOAD_FOLDER"] = 'static/resources'
 
 
 # Pagina de Bienvenida
@@ -25,23 +25,128 @@ def Home():
 
 
 # Pagina para iniciar sesión
-@app.route('/sesion/', methods=['GET','POST'])
+@app.route('/sesion/', methods=['GET', 'POST'])
 def sesion():
-    form = FormSesion()
-    if(form.validate_on_submit()):
-        flash('Damos la bienvenida al usuario {}'.format(form.usuarioSesion.data))
-        return redirect(url_for('gestor'))
-    return render_template('sesion.html', titulo='Iniciar Sesión', form=form)
+    #try:
+        if request.method == 'POST':
+            form = FormSesion()
+            db = get_db()
+            error = None
+            username = request.form['usuarioSesion']
+            password = request.form['claveSesion']
+
+            if not username:
+                error ='Debes ingresar el usuario'
+                flash(error)
+                return render_template('sesion.html', titulo='Iniciar Sesión', form=form)
+            
+            if not password:
+                error = 'contraseña requerida'
+                flash(error)
+                return render_template('sesion.html', titulo='Iniciar Sesión', form=form)
+            
+            user = db.execute('SELECT * FROM Usuarios WHERE usuario = ? AND clave = ?',(username, password)).fetchone()
+            print(user)
+            if user is None:
+                error = 'usuario o contraseña inválidos'
+            else:
+                return redirect(url_for('gestor'))
+            flash(error)
+        form = FormSesion()
+        return render_template('sesion.html', titulo='Iniciar Sesión', form=form)
+    #except:
+    #    form = FormSesion()
+    #    return render_template('sesion.html', titulo='Iniciar Sesión', form=form)
 
 
 # Pagina para registar usuario
-@app.route('/registro/', methods=['GET','POST'])
+@app.route('/registro/', methods=['GET', 'POST'])
 def registro():
-    form = FormRegistro()
-    if(form.validate_on_submit()):
-        flash('Se ha registrado el usuario {}'.format(form.usuarioRegistro.data))
-        return redirect(url_for('gracias'))
-    return render_template('registro.html', titulo='Registrar Usuario', form=form)
+    #try:
+        if request.method == 'POST':
+            form = FormRegistro()
+            db = get_db()
+            error = None
+            name = request.form['nombres']
+            lastname = request.form['apellidos']
+            email = request.form['correoRegistro']
+            username = request.form['usuarioRegistro']
+            password = request.form['claveRegistro']
+
+            if not name:
+                error ='Debes ingresar tu nombre'
+                flash(error)
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            
+            if not lastname:
+                error = 'Debes ingresar tus apellidos'
+                flash(error)
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            
+            if not email:
+                error = 'Debes ingresar tu correo'
+                flash(error)
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            
+            if not username:
+                error ='Debes ingresar el usuario'
+                flash(error)
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            
+            if not password:
+                error = 'contraseña requerida'
+                flash(error)
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            
+            if db.execute( 'SELECT * FROM Usuarios WHERE correo = ?', (email,) ).fetchone() is not None:
+                error = 'El correo {} ya existe'.format( email )
+                flash( error )
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+
+            db.execute(
+                'INSERT INTO Usuarios (nombre, apellidos, usuario, correo, clave) VALUES (?,?,?,?,?)',
+                (name, lastname, username, email, password)
+            )
+            db.commit()
+
+            if utils.isEmailValid(email):         
+                if utils.isUsernameValid(username):            
+                    yag = yagmail.SMTP('cdvitola@uninorte.edu.co','Jesuischriss_25')
+                    yag.send(to=email,subject='Activar cuenta',
+                    contents="""
+                    Bienvenido/a  """+ name +""":
+
+
+                    Te agradecemos por ser parte de nuestra gran red de imagenes.
+
+                    Usa este link para activar tu cuenta y gozar de nuestro servicios:
+
+                    https://www.avenidasiemprevivacallefalsa123.com.co
+
+                    Que tengas un resto de dia muy agradable.
+
+
+                    Atentamente,
+
+                           La Administracion.""")
+                else:
+                    flash( "Usuario no valido " + name ) 
+                    return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            else:
+                flash( "Correo no valido " + name )
+                return render_template('registro.html', titulo='Registrar Usuario', form=form)
+            # yag = yagmail.SMTP('micuenta@gmail.com', 'clave') #modificar con tu informacion personal
+            # yag.send(to=email, subject='Activa tu cuenta',
+            #        contents='Bienvenido, usa este link para activar tu cuenta ')
+            #mensaje = 'Revisa tu correo para activar tu cuenta'
+            flash( 'Revisa tu correo para activar tu cuenta' )
+            form = FormSesion()
+            return redirect(url_for('sesion'))
+        form = FormRegistro()
+        return render_template('registro.html', titulo='Registrar Usuario', form=form)
+    #except:
+    #    form = FormSesion()
+    #    return render_template('registro.html', titulo='Registrar Usuario', form=form)
 
 
 # Pagina para recuperar contraseña
@@ -49,12 +154,24 @@ def registro():
 def recuperar():
     form = FormRecuperar()
     if(form.validate_on_submit()):
-        email = form.correoRecuperar.data
+        email = request.form['correoRecuperar']
+        db = get_db()
+        error = None
+        #email = form.correoRecuperar.data
+        if not email:
+            error = 'Debes ingresar tu correo'
+            flash(error)
+            return render_template('recuperarClave.html', titulo='Recuperar Clave', form=form)
+        if db.execute( 'SELECT * FROM Usuarios WHERE correo = ?', (email,) ).fetchone() is None:
+            error = 'El correo {} no hace parte de nuestras base de datos'.format( email )
+            flash( error )
+            return render_template('recuperarClave.html', titulo='Recuperar Clave', form=form)
         if utils.isEmailValid(email):
             yag = yagmail.SMTP('cdvitola@uninorte.edu.co','Jesuischriss_25')
             yag.send(to=email,subject='Restablecer Contraseña',
             contents="""
             Hola, querido usuario:
+
 
             Te hemos enviado un enlace para que puedas restablecer tu contraseña.
 
@@ -62,9 +179,10 @@ def recuperar():
 
             Que tengas un resto de dia muy agradable.
 
+
             Atentamente,
 
-            La Administracion.""")
+                   La Administracion.""")
             flash('Se ha enviado un enlace de "restablecer contraseña" a tu correo {}'.format(form.correoRecuperar.data))
             return redirect(url_for('gracias'))
         else:
@@ -92,20 +210,19 @@ def crear():
     return render_template('crear.html', titulo='Crear Imagen', form=form)
 
 # Pagina para cuando la imagen creada se sube al proyecto
-@app.route('/gestor/crear/imagensubida', methods=('GET', 'POST') )
+@app.route('/gestor/crear/imagensubida/', methods=('GET', 'POST') )
 #@login_required
 def imagenSubidaCr():
     path = ''
     form = FormCrear()
     if request.method == 'POST':
-        archivo = form.crearArchivo.data
-        filename = secure_filename(archivo.filename) # obtener el nombre del archivo de forma segura.
-        path = os.path.join(app.config["FOLDER_CARGA"], filename) # ruta de la imagen, incluyendola.
-        archivo.save(path)
-        path = os.path.join('static/resources/', filename)
+        file = request.files['crearArchivo']
+        filename = secure_filename(file.filename) # obtener el nombre del archivo de forma segura.
+        path = os.path.join(app.config["UPLOAD_FOLDER"],filename) # ruta de la imagen, incluyendola.
+        file.save(path)
         flash( 'Imagen guardada con éxito.' )
-        return render_template("imagenSubidaCr.html", nombrearchivo=filename, path=path)
-    return redirect(url_for('crearnew'))
+    return render_template('imagenSubidaCr.html', nombrearchivo=filename, path=path)
+    #return redirect(url_for('crear'))
 
 
 # Pagina para actualizar imagenes
@@ -118,20 +235,19 @@ def actualizar():
     return render_template('actualizar.html', titulo='Actualizar Imagen', form=form)
 
 # Pagina para cuando la imagen actualizada se sube al proyecto
-@app.route('/gestor/actualizar/imagensubida', methods=('GET', 'POST') )
+@app.route('/gestor/actualizar/imagensubida/', methods=('GET', 'POST') )
 #@login_required
 def imagenSubidaAct():
     path = ''
     form = FormActualizar()
     if request.method == 'POST':
-        archivo = form.actulzarArchivo.data
-        filename = secure_filename(archivo.filename) # obtener el nombre del archivo de forma segura.
-        path = os.path.join(app.config["FOLDER_CARGA"], filename) # ruta de la imagen, incluyendola.
-        archivo.save(path)
-        path = os.path.join('static/resources/', filename)
+        file = request.files['actulzarArchivo']
+        filename = secure_filename(file.filename) # obtener el nombre del archivo de forma segura.
+        path = os.path.join(app.config["UPLOAD_FOLDER"], filename) # ruta de la imagen, incluyendola.
+        file.save(path)
         flash( 'Imagen actualizada con éxito.' )
-        return render_template("imagenSubidaAct.html", nombrearchivo=filename, path=path)
-    return redirect(url_for('actualizar'))
+    return render_template("imagenSubidaAct.html", nombrearchivo=filename, path=path)
+    #return redirect(url_for('actualizar'))
 
 
 # Pagina para eliminar imagenes
